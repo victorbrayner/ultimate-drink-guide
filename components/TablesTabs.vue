e6e237d07686ef91cac397f59cf78b651a9b0140
Victor Brayner <victorbrayner@gmail.com>
2023-08-07 10:38:01 -0300

    feat: add tabs for results and favorites tables

---
+++ components/TablesTabs.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-tabs
+      v-model="tab"
+      fixed-tabs
+    >
+      <v-tab value="one">RESULTS</v-tab>
+      <v-tab value="two">FAVORITES</v-tab>
+    </v-tabs>
+    <v-window v-model="tab">
+      <v-window-item key="one" value="one">
+        <drinks-table table-mode="search" />
+      </v-window-item>
+      <v-window-item key="two" value="two">
+        <drinks-table table-mode="favorites" />
+      </v-window-item>
+    </v-window>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const tab = ref(0)
+</script>

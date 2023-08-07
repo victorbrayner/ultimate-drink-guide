caec01bce3107b9e16b34d4f18ff47942edc7478
Victor Brayner <victorbrayner@gmail.com>
2023-08-07 10:30:49 -0300

    feat: add index and error pages

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <v-container>
+      <NuxtPage />
+    </v-container>
   </div>
 </template>
+
+<style lang="sass">
+  body 
+    background: linear-gradient(to bottom, #ff3737, #d20048, #9d004c, #660044, #330131) no-repeat
+    background-size: cover
+    background-position: center
+    height: 150vh
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-container >
+      <v-card
+        class="mx-auto elevation-20 rounded-xl"
+        max-width="700"
+        title="Ultimate Drink Guide"
+        prepend-icon="mdi-glass-cocktail"
+      >
+        <v-container>
+          <search />
+        </v-container>
+        <v-container>
+          <tables-tabs />
+        </v-container>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useDrinksStore } from '../store/drinks';
+import { onMounted } from "vue";
+
+const store = useDrinksStore();
+
+onMounted(() => {
+  store.fetchSavedDrinks()
+  store.fetchCategories()
+});
+
+</script>
